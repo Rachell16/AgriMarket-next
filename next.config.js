291bba6b60d2c6a68ee52a8b4742a0a1df917758
Rachell16 <racheltobing09@gmail.com>
2026-05-22 00:30:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['your-project.supabase.co'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
